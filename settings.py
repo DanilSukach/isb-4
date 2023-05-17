3b0a8ef75a0c439de38b097dccc35abd9eb579de
DanilSukach <danil211-1@yandex.ru>
2023-05-17 10:57:41 +0400

    Добавил файл с настройками, файл с BIN, hash карты

---
+++ settings.py
@@ -0,0 +1,14 @@
+import json
+
+SETTINGS = {
+    'card_number_file': 'files/card_number.txt',
+    'statistics_file': 'files/statistics.txt',
+    'hash_file': 'files/hash.txt',
+    'bins_file': 'files/bin.txt',
+    'last_numbers_file': 'files/last_numbers.txt',
+    'visual_directory': 'files'
+}
+
+if __name__ == '__main__':
+    with open('files/settings.json', 'w') as fp:
+        json.dump(SETTINGS, fp)
